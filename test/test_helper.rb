e0d1974f46d426fcf80c4544555bc26bf1668233
yui-har <7232538+yui-har@users.noreply.github.com>
2022-05-17 09:16:40 +0900

    Appear note templates by roles visibility(0.3-stable) (#28)
    * Create fixtures
    
    * Change fixture setting
    
    * Add unit tests for NoteTemplate#loadable
    
    * Allow admin users to load note templates
    
    * Create functional tests for NoteTemplatesController
    
    * Don't appear note templates that aren't loadable

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -29,4 +29,5 @@ end
 require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
 ActiveRecord::FixtureSet.create_fixtures(File.dirname(__FILE__) + '/fixtures/',
                                          %i[issue_templates issue_template_settings
-                                            global_issue_templates global_issue_templates_projects])
+                                            global_issue_templates global_issue_templates_projects
+                                            note_templates note_visible_roles])
