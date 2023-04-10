6021a504c566564baa0038788b39fa8dda820cd1
yurie-mizushima <mizushima@agileware.jp>
2023-04-10 17:18:19 +0900

    1.1.2

--- init.rb
+++ init.rb
@@ -47,7 +47,7 @@ Redmine::Plugin.register :redmine_issue_templates do
     name 'Redmine Issue Templates plugin'
     author 'Agileware Inc.'
     description 'Plugin to generate and use issue templates for each project to assist issue creation.'
-    version '1.1.1'
+    version '1.1.2'
     author_url 'https://agileware.jp/'
     requires_redmine version_or_higher: '4.0'
     url 'https://github.com/agileware-jp/redmine_issue_templates'
